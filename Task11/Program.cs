9682eb8fcf230ed64b29ea556ecc75f3c46fd0cc
Polina <p-shakulo@ya.ru>
2023-10-13 06:58:32 +0300

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string line = Console.ReadLine();
+    int number;
+    number = int.Parse(line);
+    return number;
+}
+int InputNumber = ReadInt("Введите число => ");
+if (InputNumber > 0 && InputNumber < 100)
+{
+    Console.WriteLine($"У введенного числа {InputNumber} третьей цифры нет");
+}
+
+else if (InputNumber > 99 && InputNumber < 1000)
+{
+    int Digital = InputNumber % 10;
+    Console.WriteLine($"Число {InputNumber} имеет третью цифру = {Digital}");
+}
+ else if (InputNumber > 999 && InputNumber < 10000)
+{
+ int temp = InputNumber / 10;
+ int Digital = temp % 10;
+Console.WriteLine($"Число {InputNumber} имеет третью цифру = {Digital}");
+
+ }
+
+
---
+++ Task12/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string line = Console.ReadLine();
+    int number;
+    number = int.Parse(line);
+    return number;
+}
+int Day = ReadInt("Введите цифру от 1 до 7 => ");
+if(Day >= 1 & Day <= 5)
+{
+    Console.WriteLine($"Этот день НЕ является выходным");
+
+}
+    
+if(Day == 6 || Day == 7)
+{
+    Console.WriteLine($"Этот день является выходным");
+}
+if(Day < 1 || Day >= 8)
+{
+    Console.WriteLine($"Введена некорректная цифра");
+}
